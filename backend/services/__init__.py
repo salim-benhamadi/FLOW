ea54fc3621865c285bf7b986ee43185f370425ae
Benhamadi Salim (ATV SP D PD PTE PAD PRE APS) <salim.benhamadi@infineon.com>
2025-06-08 15:21:06 +0200

    fix model file path

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -2,13 +2,11 @@
 Services module for business logic
 """
 
-from .model_service import ModelService
 from .metrics_service import MetricsService
 from .feedback_service import FeedbackService
 from .settings_service import SettingsService
 
 __all__ = [
-    'ModelService',
     'MetricsService',
     'FeedbackService',
     'SettingsService'
